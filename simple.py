43f974bc2a84fb8d1ad09ada146746abd89c0898
Erick C <44537640+ecevallos264@users.noreply.github.com>
2021-07-05 16:59:44 +0000

    Assingment-00

--- simple.py
+++ simple.py
@@ -1,3 +1,4 @@
+import math
 '''
 Write 6 print() statements to "draw" the following on the console
 
@@ -10,12 +11,12 @@ xxxxxxxx
 
 (hint: use escape sequence)
 '''
-line1=''
-line2=''
-line3=''
-line4=''
-line5=''
-line6=''
+line1 = 'ooo/\\ooo'
+line2 = 'oo/  \\oo'
+line3 = 'o/ \'\' \\o'
+line4 = '/______\\'
+line5 = '|| || ||'
+line6 = 'xxxxxxxx'
 print(line1)
 print(line2)
 print(line3)
@@ -23,16 +24,22 @@ print(line4)
 print(line5)
 print(line6)
 
+string = [
+    'ooo/\\ooo\n', 'oo/  \\oo\n', 'o/ \'\' \\o\n', '/______\\\n',
+    '|| || || \n', 'xxxxxxxx'
+]
 '''
 Can you do it with just one print statement?
 (hint: use string concatenation
 '''
-print()
-
+str = ""
+for i in range(len(string)):
+    str += string[i]
+print(str)
 '''
 Choose three positive values for a, b, and c such that the final print() statement returns True
 '''
-a = 
-b = 
-c = 
+a = 3
+b = 4
+c = 5
 print(a**2 + b**2 == c**2)
